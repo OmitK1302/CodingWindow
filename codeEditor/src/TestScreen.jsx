da9fadd64f282ffe1dc12ba32a6c09775ca59af1
OmitK1302 <omitkumar1854@gmail.com>
2025-07-06 00:39:58 +0530

    Work distributed

---
+++ codeEditor/src/TestScreen.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const CodingEditor = () => {
+  return (
+    <div>
+      <nav>
+
+      </nav>
+
+      <div>
+        {/* Aryan Tera kaam niche wale div m */}
+        <div>
+
+        </div>
+
+        {/* Ye mera */}
+        <div>
+
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default CodingEditor
